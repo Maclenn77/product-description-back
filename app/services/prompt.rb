c88b7ae903c3a33ac3c475c3cea4ab66c880f0e5
Juan Perez <juan.perez@bigcommerce.com>
2023-07-14 11:51:08 -0600

    refactor: refactor-prompt Add a prompt builder

--- app/services/prompt.rb
+++ app/services/prompt.rb
@@ -2,11 +2,48 @@
 require_relative 'builders/prompt_builder'
 
 class Prompt
-  def initialize
+  attr_reader :config
+  attr_accessor :prompt
+
+  # Create a prompt
+  def initialize(config)
+    @config = config
+    @prompt = base_prompt
   end
 
-  private
+  def final_prompt = prompt
 
   def base_prompt
+    text = config.dig(:description, :usage) || "sell pitch"
+
+    "Create a #{text} for a product named #{config.dig(:product, :name)}"
+  end
+
+  def with_base_description
+    @prompt += " that is #{config.dig(:product, :base_description)}"
+  end
+
+  def with_categories
+    @prompt += ". Product belongs to #{config.dig(:product, :categories).join(' ')} categories"
+  end
+
+  def with_characteristics 
+    @prompt += " and has the following characteristics: #{config.dig(:product, :characteristics)}"
+  end
+
+  def with_segment
+    @prompt += ". Text is targeted to #{config.dig(:description, :segment)}"
+  end
+
+  def with_season
+    @prompt += " for #{config.dig(:description, :season)}"
+  end
+
+  def with_keywords
+    @prompt += " using these keywords: #{config.dig(:description, :keywords).join(' ')}"
+  end
+
+  def with_language
+    @prompt += ". Answer in #{config.dig(:description, :language)}"
   end
 end
